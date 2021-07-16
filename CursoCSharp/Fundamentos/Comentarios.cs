7df7021b5fa856502e6dcf802335ce5e4f315406
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-07-16 15:54:31 -0300

    Initial commit

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// comentarios grandes, apenas digite 3 barras!
+    /// <see cref=""/> //aqui podemos lindar outro comentario
+    /// </summary>
+    class Comentarios
+    {
+        public static void Executar(){
+            //Cuidado com os comentarios desnecessarios
+            Console.WriteLine("Código Claro é sempre melhor!");
+
+            Console.WriteLine("o C# tem o XML Comments");
+        }
+    }
+}
