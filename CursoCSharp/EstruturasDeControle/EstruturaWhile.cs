0f84a314327dca6a7ace782c0e88ed0962b8c134
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-08-01 09:23:16 -0300

    Estrutura de controle - estrutura While

---
+++ CursoCSharp/EstruturasDeControle/EstruturaWhile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    class EstruturaWhile
+    {
+        public static void Executar() {
+            int palpite = 0;
+            Random ramdom = new Random();
+
+            int numeroSecreto = ramdom.Next(1, 16);
+            bool numeroEncontrado = false;
+            int tentativasRestantes = 5;
+            int tentativas = 0;
+
+            while(tentativasRestantes > 0 && !numeroEncontrado){
+                Console.WriteLine("Insira o seu palpite: ");
+                string entrada = Console.ReadLine();
+                int.TryParse(entrada, out palpite);
+
+                tentativas++;
+                tentativasRestantes--;
+
+                if(numeroSecreto == palpite) {
+                    numeroEncontrado = true;
+                    var corAnterior = ConsoleColor.Green;
+                    Console.WriteLine("Numero encontrado em {0} tentativas", tentativas);
+                    Console.BackgroundColor = corAnterior;
+                  }else if(palpite > numeroSecreto) {
+                    Console.WriteLine("Menor...Tente novamente!");
+                    Console.WriteLine("Tentativas restantes: {0} ", tentativasRestantes);
+                }else {
+                    Console.WriteLine("Maior...tente novamente!");
+                    Console.WriteLine("Tentivas restantes: {0} ", tentativasRestantes);
+                }
+            }
+        }
+    }
+}
