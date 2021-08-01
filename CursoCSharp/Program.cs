85555792912c353cbed1107a3f4d8ec75f5fec9a
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-08-01 11:02:13 -0300

    Estrutura de controle - For

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -32,6 +32,7 @@ namespace CursoCSharp {
                 {"Estrutura Switch- Estruturas de controle", EstruturaSwitch.Executar},
                 {"Estrutura While- Estruturas de controle", EstruturaWhile.Executar},
                 {"Estrutura Do While- Estruturas de controle", EstruturaDoWhile.Executar},
+                {"Estrutura For - Estruturas de controle", EstruturaFor.Executar},
             });
 
             central.SelecionarEExecutar();
