cb9484141577c789d4ea8282d6a939e463d8b903
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-08-05 21:59:54 -0300

    Método com retorno - Classes e Métodos

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -41,6 +41,7 @@ namespace CursoCSharp {
                 //Classes e métodos
                 {"Membros - Classes e Metodos", Membros.Executar},
                 {"Construtores - Classes e Metodos", Construtores.Executar},
+                {"Metodo com retorno - Classes e Metodos", MetodosComRetorno.Executar},
             });
 
             central.SelecionarEExecutar();
