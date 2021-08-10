1b4cc190e42ee19c79447cf112443bc3d4c12de8
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-08-10 20:24:53 -0300

    Params - Classes e Métodos

---
+++ CursoCSharp/ClassesEMetodos/Params.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Params{
+
+        /*
+         * params é uma palavra reservada do C# permite passar parametros variaveis.
+         * no nosso caso estamos passando um Array de strings.
+         */
+
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine("Olá {0}", pessoa);
+            }
+        }
+        public static void Executar(){
+            Recepcionar("Flavio", "Leandro", "Celso", "Gabriel", "Rafael", "Marcos", "Eduardo");
+        }
+   }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -45,6 +45,7 @@ namespace CursoCSharp {
                 {"Metodo Estáticos - Classes e Métodos", MetodosEstaticos.Executar},
                 {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar},
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
+                {"Params - Classes e Métodos", Params.Executar},
             });
 
             central.SelecionarEExecutar();
