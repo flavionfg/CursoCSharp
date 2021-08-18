7b91a4b2c448fb6e9ba82fc1718512f6cdea57f1
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-08-17 21:05:43 -0300

    Construtor This - OO

---
+++ CursoCSharp/OO/ConstrutorThis.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.OO
+{
+
+    public class Animal
+    {
+        public string Nome { get; set; }
+
+        public Animal(string nome)
+        {
+            Nome = nome;
+        }
+    }
+
+    public class Cachorro : Animal
+    {
+        public double Altura { get; set; }
+
+        public Cachorro(string nome) : base(nome)
+        {
+            Console.WriteLine($"Cachorro {nome} inicializado");
+        }
+
+        public Cachorro(string nome, double altura) : this(nome) //o This esta chamando o construtor base para setar o nome.
+        {
+            Altura = altura;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} tem {Altura}cm de altura!"; //Sem este método na hora de printar a classe vai aparecer o caminho dela e não o valor.
+        }
+    }
+
+    class ConstrutorThis
+    {
+        public static void Executar()
+        {
+            var spike = new Cachorro("Spike");
+            var caramelo = new Cachorro("Max", 40.0);
+
+            Console.WriteLine(spike);
+            Console.WriteLine(caramelo);
+            //Console.WriteLine(caramelo.ToString());
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -71,6 +71,7 @@ namespace CursoCSharp {
 
                 //OO
                 {"Herança - OO", Heranca.Executar},
+                {"Construtor This - OO", ConstrutorThis.Executar},
             });
 
             central.SelecionarEExecutar();
