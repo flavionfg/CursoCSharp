7e20f87f3df5e577cf8698045e999b425e07586a
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-08-19 12:32:56 -0300

    [FIX] Encapsulamento - OO
    Faltou commitar a classe SubCelebridade do Exemplo Encapsulamento - OO.

---
+++ Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    public class SubCelebridade
+    {
+        //Todos
+        public string InfoPublica = "Tenho um instagram!";
+
+        //Heranca
+        protected string CorDoOlho = "Verde";
+
+        //mesmo projeto (assembly)
+        internal ulong NumeroCelular = 551199999999;
+
+        //Heranca ou mesmo projeto
+        protected internal string JeitoDeFalar = "Uso muitas girias";
+
+        //mesma class ou heranca no mesmo projeto (só funciona na versão do c# >= 7.2)
+        private protected string SegredoFamilia = "Bla bla";
+
+        //private é o padrão 
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("SubCelebridade...");
+
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
