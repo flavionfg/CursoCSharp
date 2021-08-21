ee2605b83659e338876ad56b4d87e6c60f2b96e1
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-08-21 09:15:15 -0300

    Nullables - Tópicos Avançados

---
+++ CursoCSharp/TopicosAvancados/Nullables.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    class Nullables{
+        //static int num3; variaveis em nivel de classe são inicializadas por padrão. padrao de int é 0.
+        public static void Executar(){
+            //Opçao 1
+            Nullable<int> num1 = null;
+
+            //Opçao 2
+            int? num2 = null;
+
+            if (num1.HasValue){
+                Console.WriteLine("Valor de num: {0}", num1);
+            }else{
+                Console.WriteLine("A variavel não possui valor.");
+            }
+
+            int valor = num1 ?? 1000;
+            Console.WriteLine(valor);
+
+            bool? booleana = null;
+            bool resultado = booleana.GetValueOrDefault();
+            Console.WriteLine(resultado);
+
+            try{
+                int x = num1.GetValueOrDefault();
+                int y = num2.GetValueOrDefault();
+                Console.WriteLine(x + y);
+
+            }catch(Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
